c6b57779c7f8c1ea6be16620af5ad5ea6dc82c51
Calvin <CalvinNelson@Administrators-MacBook-Pro.local>
2024-12-14 18:54:25 -0500

    first commit

---
+++ Note-taking-app/app.js
@@ -0,0 +1,10 @@
+const validator = require('validator')
+const gerNotes = require('https/mead.io')
+
+const msg = getNotes(
+    console.log(msg)
+    
+    console.log (validator.isURL('https/mead.io'))
+)
+
+
